23740575c2368aed32cfa7bfa1a6b21ad1fd17c0
Emil Brink <emil@obsession.se>
2013-07-19 16:42:07 +0300

    Added delete_event handling to main window.

--- src/gui.c
+++ src/gui.c
@@ -4,12 +4,20 @@
 
 #include "gui.h"
 
+static gboolean evt_mainwindow_delete(GtkWidget *wid, const GdkEvent *evt, gpointer user)
+{
+	gtk_main_quit();
+
+	return TRUE;
+}
+
 GtkWidget * gui_mainwindow_open(const char *title)
 {
 	GtkWidget	*win;
 
 	win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(win), title);
+	g_signal_connect(G_OBJECT(win), "delete_event", G_CALLBACK(evt_mainwindow_delete), NULL);
 
 	return win;
 }
