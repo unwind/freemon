34c251686ebcb12af8773ccdb1bdc7f5f4ce8c43
Emil Brink <emil@obsession.se>
2013-07-24 19:54:23 +0200

    Added a "Detect" button for auto-detecting connected FRDM board.
    This will be nice.

--- src/action-connect.c
+++ src/action-connect.c
@@ -20,13 +20,13 @@ static void evt_connect_activate(GtkAction *action, gpointer user)
 	gtk_window_set_title(GTK_WINDOW(dlg), "Connect to FRDM board");
 
 	GtkWidget *grid = gtk_grid_new();
-	gtk_grid_set_column_spacing(GTK_GRID(grid), 10);
+	gtk_container_set_border_width(GTK_CONTAINER(grid), 6);
 	GtkWidget *label = gtk_label_new("Name:");
 	gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 1, 1);
 	GtkWidget *name = gtk_entry_new();
 	gtk_widget_set_hexpand(name, TRUE);
 	gtk_widget_set_halign(name, GTK_ALIGN_FILL);
-	gtk_grid_attach(GTK_GRID(grid), name, 1, 0, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid), name, 1, 0, 2, 1);
 
 	label = gtk_label_new("Serial Device:");
 	gtk_grid_attach(GTK_GRID(grid), label, 0, 1, 1, 1);
@@ -40,6 +40,9 @@ static void evt_connect_activate(GtkAction *action, gpointer user)
 	gtk_widget_set_hexpand(upload, TRUE);
 	gtk_widget_set_halign(upload, GTK_ALIGN_FILL);
 	gtk_grid_attach(GTK_GRID(grid), upload, 1, 2, 1, 1);
+
+	GtkWidget *detect = gtk_button_new_with_label("Detect");
+	gtk_grid_attach(GTK_GRID(grid), detect, 2, 1, 1, 2);
 
 	gtk_container_add(GTK_CONTAINER(gtk_dialog_get_content_area(GTK_DIALOG(dlg))), grid);
 	gtk_widget_show_all(grid);
