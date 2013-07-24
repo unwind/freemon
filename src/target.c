b233bd018bdf4eb8bc950f257e19d8e953ece45a
Emil Brink <emil@obsession.se>
2013-07-24 13:16:49 +0200

    Moved the terminal right-click menu over to the target module.

--- src/target.c
+++ src/target.c
@@ -19,6 +19,7 @@ struct Target {
 
 	GtkWidget	*gui;
 	GtkWidget	*terminal;
+	GtkWidget	*terminal_menu;
 };
 
 /* ------------------------------------------------------------------- */
@@ -39,6 +40,26 @@ Target * target_new_serial(const char *name, const char *tty_device, const char 
 
 /* ------------------------------------------------------------------- */
 
+static void evt_terminal_reset_activate(GtkWidget *wid, gpointer user)
+{
+	vte_terminal_reset(VTE_TERMINAL(((Target *) user)->terminal), TRUE, TRUE);
+}
+
+static gboolean evt_terminal_button_press(GtkWidget *wid, GdkEvent *event, gpointer user)
+{
+	const GdkEventButton	*btn = (GdkEventButton *) event;
+
+	if(btn->button == 3)
+	{
+		Target	*target = user;
+
+		gtk_menu_popup(GTK_MENU(target->terminal_menu), NULL, NULL, NULL, NULL, btn->button, btn->time);
+
+		return TRUE;
+	}
+	return FALSE;
+}
+
 GtkWidget * target_gui_create(Target *target)
 {
 	if(target->gui != NULL)
@@ -53,13 +74,19 @@ GtkWidget * target_gui_create(Target *target)
 	target->terminal = vte_terminal_new();
 	vte_terminal_set_size(VTE_TERMINAL(target->terminal), 80, 25);
 	gtk_widget_add_events(target->terminal, GDK_KEY_PRESS_MASK);
-/*	g_signal_connect(G_OBJECT(target->terminal), "button-press-event", G_CALLBACK(evt_terminal_button_press), info);
-	g_signal_connect(G_OBJECT(target->terminal), "key-press-event", G_CALLBACK(evt_terminal_key_press), info);
+	g_signal_connect(G_OBJECT(target->terminal), "button-press-event", G_CALLBACK(evt_terminal_button_press), target);
+/*	g_signal_connect(G_OBJECT(target->terminal), "key-press-event", G_CALLBACK(evt_terminal_key_press), info);
 	g_signal_connect(G_OBJECT(target->terminal), "map", G_CALLBACK(evt_terminal_mapped), info);
 */	gtk_widget_set_vexpand(target->terminal, TRUE);
 	gtk_grid_attach(GTK_GRID(target->gui), target->terminal, 0, 1, 5, 1);
 	GtkWidget *scbar = gtk_scrollbar_new(GTK_ORIENTATION_VERTICAL, gtk_scrollable_get_vadjustment(GTK_SCROLLABLE(target->terminal)));
 	gtk_grid_attach(GTK_GRID(target->gui), scbar, 5, 1, 1, 1);
+
+	target->terminal_menu = gtk_menu_new();
+	GtkWidget *item = gtk_menu_item_new_with_label("Reset");
+	g_signal_connect(G_OBJECT(item), "activate", G_CALLBACK(evt_terminal_reset_activate), target);
+	gtk_menu_shell_append(GTK_MENU_SHELL(target->terminal_menu), item);
+	gtk_widget_show_all(item);
 
 	return target->gui;
 }
