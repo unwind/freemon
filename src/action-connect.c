1a53936727135275ce1d34f2c25e701b9e737b5f
Emil Brink <emil@obsession.se>
2013-07-27 18:58:34 +0200

    Implemented sensitivity-setting for the OK button.
    Not very ambitious at the moment, but better than nothing.

--- src/action-connect.c
+++ src/action-connect.c
@@ -2,6 +2,7 @@
  * frdm-mntr connect action module.
 */
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -13,6 +14,7 @@ typedef struct {
 	GtkWidget	*name;
 	GtkWidget	*device;
 	GtkWidget	*path;
+	GtkWidget	*dialog;
 } ConnectInfo;
 
 /* ------------------------------------------------------------------- */
@@ -27,12 +29,30 @@ static void evt_autodetect_clicked(GtkWidget *wid, gpointer user)
 	printf("hello\n");
 }
 
+static void evaluate_entries(const ConnectInfo *info)
+{
+	const bool	device_set = gtk_entry_get_text_length(GTK_ENTRY(info->device)) > 0;
+	const bool	path_set = gtk_entry_get_text_length(GTK_ENTRY(info->device)) > 0;
+
+	gtk_dialog_set_response_sensitive(GTK_DIALOG(info->dialog), GTK_RESPONSE_OK, device_set && path_set);
+}
+
+static void evt_device_changed(GtkWidget *wid, gpointer user)
+{
+	evaluate_entries(user);
+}
+
+static void evt_path_changed(GtkWidget *wid, gpointer user)
+{
+	evaluate_entries(user);
+}
+
 static void evt_connect_activate(GtkAction *action, gpointer user)
 {
 	ConnectInfo	info;
 
-	GtkWidget *dlg = gtk_dialog_new();
-	gtk_window_set_title(GTK_WINDOW(dlg), "Connect to FRDM board");
+	info.dialog = gtk_dialog_new();
+	gtk_window_set_title(GTK_WINDOW(info.dialog), "Connect to FRDM board");
 
 	GtkWidget *grid = gtk_grid_new();
 	gtk_container_set_border_width(GTK_CONTAINER(grid), 6);
@@ -46,6 +66,7 @@ static void evt_connect_activate(GtkAction *action, gpointer user)
 	label = gtk_label_new("Serial Device:");
 	gtk_grid_attach(GTK_GRID(grid), label, 0, 1, 1, 1);
 	info.device = gtk_entry_new();
+	g_signal_connect(G_OBJECT(info.device), "changed", G_CALLBACK(evt_device_changed), &info);
 	gtk_grid_attach(GTK_GRID(grid), info.device, 1, 1, 1, 1);
 
 	label = gtk_label_new("Mounted As:");
@@ -53,21 +74,24 @@ static void evt_connect_activate(GtkAction *action, gpointer user)
 	info.path = gtk_file_chooser_button_new("Select Upload Directory", GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER);
 	gtk_widget_set_hexpand(info.path, TRUE);
 	gtk_widget_set_halign(info.path, GTK_ALIGN_FILL);
+	g_signal_connect(G_OBJECT(info.path), "file-set", G_CALLBACK(evt_path_changed), &info);
 	gtk_grid_attach(GTK_GRID(grid), info.path, 1, 2, 1, 1);
 
 	GtkWidget *detect = gtk_button_new_with_label("Auto-\nDetect");
 	g_signal_connect(G_OBJECT(detect), "clicked", G_CALLBACK(evt_autodetect_clicked), &info);
 	gtk_grid_attach(GTK_GRID(grid), detect, 2, 1, 1, 2);
 
-	gtk_container_add(GTK_CONTAINER(gtk_dialog_get_content_area(GTK_DIALOG(dlg))), grid);
+	gtk_container_add(GTK_CONTAINER(gtk_dialog_get_content_area(GTK_DIALOG(info.dialog))), grid);
+
+	gtk_dialog_add_button(GTK_DIALOG(info.dialog), GTK_STOCK_OK, GTK_RESPONSE_OK);
+	gtk_dialog_add_button(GTK_DIALOG(info.dialog), GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL);
+
 	gtk_widget_show_all(grid);
+	evaluate_entries(&info);
 
-	gtk_dialog_add_button(GTK_DIALOG(dlg), GTK_STOCK_OK, GTK_RESPONSE_OK);
-	gtk_dialog_add_button(GTK_DIALOG(dlg), GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL);
+	const gint response = gtk_dialog_run(GTK_DIALOG(info.dialog));
 
-	const gint response = gtk_dialog_run(GTK_DIALOG(dlg));
-
-	gtk_widget_destroy(dlg);
+	gtk_widget_destroy(info.dialog);
 }
 
 /* ------------------------------------------------------------------- */
@@ -81,4 +105,3 @@ GtkAction * action_connect_init(GuiInfo *gui)
 
 	return upload;
 }
-
