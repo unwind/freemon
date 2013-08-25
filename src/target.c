8f08e8137d7f75b212088a6d9dc2cda16814e900
Emil Brink <emil@obsession.se>
2013-08-25 11:54:12 +0200

    Added GNU GPLv3 license block.
    Also snuck in a renaming, let's have a name that we can capitalize for once.

--- src/target.c
+++ src/target.c
@@ -1,5 +1,22 @@
 /*
- * frdm-mntr target module.
+ * Freemon: target module.
+ *
+ * Copyright 2013 Emil Brink <emil@obsession.se>.
+ * 
+ * This file is part of Freemon.
+ *
+ * Freemon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Freemon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Freemon.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <gtk/gtk.h>
--- src/tty-fifo.h
+++ src/tty-fifo.h
@@ -1,7 +1,22 @@
 /*
- * frdm-mntr FIFO TTY module.
+ * Freemon: TTY FIFO module. Never include this directly, use "tty.h".
  *
- * Not intended to be directly included, just include "tty.h".
+ * Copyright 2013 Emil Brink <emil@obsession.se>.
+ * 
+ * This file is part of Freemon.
+ *
+ * Freemon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Freemon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Freemon.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #if !defined TTY_FIFO_H_
