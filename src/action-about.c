c07a2e88c612d73e897584e7dc6fe84442a8b0ef
Emil Brink <emil@obsession.se>
2013-08-31 16:33:38 +0200

    Implemented actual About dialog.
    Very nice, thanks to GtkAboutDialog.
    
    Also defined a global version macro in the Makefile, very old-sk00l.

--- src/action-about.c
+++ src/action-about.c
@@ -23,8 +23,28 @@
 
 /* ------------------------------------------------------------------- */
 
+static void evt_about_response(GtkDialog *dlg, gint response, gpointer user)
+{
+	gtk_widget_destroy(GTK_WIDGET(dlg));
+}
+
 static void evt_about_activate(GtkAction *action, gpointer user)
 {
+	GtkAboutDialog *about = GTK_ABOUT_DIALOG(gtk_about_dialog_new());
+
+	gtk_about_dialog_set_program_name(about, "Freemon");
+	gtk_about_dialog_set_version(about, VERSION);
+	gtk_about_dialog_set_copyright(about, "Copyright 2013 Emil Brink");
+	gtk_about_dialog_set_license_type(about, GTK_LICENSE_GPL_3_0);
+	gtk_about_dialog_set_website(about, "https://github.com/unwind/freemon");
+	gtk_about_dialog_set_website_label(about, "Follow Freemon on GitHub!");
+	const char *authors[] = { "Emil Brink <emil@obsession.se>", NULL };
+	gtk_about_dialog_set_authors(about, authors);
+
+	gtk_dialog_set_default_response(GTK_DIALOG(about), GTK_RESPONSE_CLOSE);
+	g_signal_connect(G_OBJECT(about), "response", G_CALLBACK(evt_about_response), NULL);
+
+	gtk_widget_show_all(about);
 }
 
 /* ------------------------------------------------------------------- */
