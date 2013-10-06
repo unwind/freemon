e2e18e59409eee73d54368f3ccb174378e2401c5
Emil Brink <emil@obsession.se>
2013-10-06 18:46:32 +0200

    Snapshot of file monitoring.
    Completely untested, just doodling at it to get a sensible structure.
    
    It's a bit scary with resource-management risks aplenty.

--- src/action-autoupload.c
+++ src/action-autoupload.c
@@ -22,23 +22,106 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <gio/gio.h>
+
 #include "action-autoupload.h"
 
 /* ------------------------------------------------------------------- */
 
+typedef struct {
+	GtkWidget	*chooser;
+	char		filename[1024];
+	GFile		*file;
+	GFileMonitor	*monitor;
+} AutouploadInfo;
+
+/* ------------------------------------------------------------------- */
+
+static void action_info_install(GtkAction *action, GtkWidget *fch)
+{
+	AutouploadInfo *ai = g_malloc(sizeof *ai);
+
+	ai->chooser = fch;
+	ai->filename[0] = '\0';
+	ai->file = NULL;
+	ai->monitor = NULL;
+
+	g_object_set_data(G_OBJECT(action), "info", ai);
+}
+
+static AutouploadInfo * action_info_get(const GtkAction *action)
+{
+	return g_object_get_data(G_OBJECT(action), "info");
+}
+
+static bool action_monitor_install(GtkAction *action, const char *fn)
+{
+	GFile *f = g_file_new_for_path(fn);
+	if(f != NULL)
+	{
+		AutouploadInfo *info = action_info_get(action);
+		if((info->monitor = g_file_monitor_file(info->file, G_FILE_MONITOR_NONE, NULL, NULL)) != NULL)
+		{
+			info->file = f;
+			return true;
+		}
+		g_object_unref(f);
+	}
+	return false;
+}
+
+static void action_monitor_uninstall(GtkAction *action)
+{
+	AutouploadInfo	*info = action_info_get(action);
+
+	if(info->monitor != NULL)
+	{
+		g_file_monitor_cancel(info->monitor);
+		g_object_unref(info->monitor);
+		info->monitor = NULL;
+	}
+	if(info->file != NULL)
+	{
+		g_object_unref(info->file);
+		info->file = NULL;
+	}
+}
+
+/* This gets called whenever the filename in the GtkFileChooser for the binary to upload changes. */
+static void action_update_filename(GtkAction *action)
+{
+	AutouploadInfo	*info = action_info_get(action);
+
+	action_monitor_uninstall(action);
+
+	gchar *fn = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(info->chooser));
+	action_monitor_install(action, fn);
+	g_free(fn);
+}
+
 static void evt_autoupload_toggled(GtkAction *action, gpointer user)
 {
-	Target		*target = user;
+	const bool active = gtk_toggle_action_get_active(GTK_TOGGLE_ACTION(action));
+}
+
+static void evt_chooser_file_set(GtkWidget *wid, gpointer user)
+{
+	GtkAction	*action = user;
+
+	action_update_filename(action);
 }
 
 /* ------------------------------------------------------------------- */
 
-GtkAction * action_autoupload_new(Target *target, GtkWidget *filechooser)
+GtkAction * action_autoupload_new(Target *target, GtkWidget *filechooserbutton)
 {
-	GtkAction	*autoupload;
+	GtkToggleAction	*autoupload;
 
 	autoupload = gtk_toggle_action_new("autoupload", "Auto-upload", "Uploads the selected binary whenever it changes.", GTK_STOCK_MEDIA_FORWARD);
-	g_signal_connect(G_OBJECT(autoupload), "toggled", G_CALLBACK(evt_autoupload_toggled), target);
+	action_info_install(GTK_ACTION(autoupload), filechooserbutton);
 
-	return autoupload;
+	g_signal_connect(G_OBJECT(autoupload), "toggled", G_CALLBACK(evt_autoupload_toggled), target);
+	g_signal_connect(G_OBJECT(filechooserbutton), "file_set", G_CALLBACK(evt_chooser_file_set), autoupload);
+
+	return GTK_ACTION(autoupload);
 }
